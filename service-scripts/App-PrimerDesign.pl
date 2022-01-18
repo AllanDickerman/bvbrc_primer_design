46a543888cb2fbda63c629f3aef9952861380403
AllanDickerman <dickerman@virginia.edu>
2022-01-18 17:51:39 -0600

    Actually fixed interpreting input_type sequence_text.

--- service-scripts/App-PrimerDesign.pl
+++ service-scripts/App-PrimerDesign.pl
@@ -63,7 +63,7 @@ sub design_primers {
    
     run("echo $tmpdir && ls -ltr $tmpdir");
 
-    if ($params->{input_type} eq "sequence_input") {
+    if ($params->{input_type} eq "sequence_text") {
         $params->{SEQUENCE_TEMPLATE} = $params->{sequence_input};
     }
     elsif ($params->{input_type} eq "workspace_fasta") {
